1e10ccaaa4f6904decceeb3c55e511e3dfd81023
unknown <mstiff1983@gmail.com>
2020-07-16 13:52:45 -0400

    Created the Address Model

---
+++ CampRealEstate/Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampRealEstate.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        [Display(Name = "Street Address:")]
+        public string StreetAddress { get; set; }
+
+        [Display(Name = "City:")]
+        public string City { get; set; }
+
+        [Display(Name = "State:")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code:")]
+        public double ZipCode { get; set; }
+
+        [Display(Name = "Latitude:")]
+        public double Latitude { get; set; }
+
+        [Display(Name = "Longitude:")]
+        public double Longitude { get; set; }
+    }
+}

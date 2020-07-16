3f175aeb13ead4b2d29d6cfebf1b97b4716ef070
unknown <mstiff1983@gmail.com>
2020-07-16 13:45:21 -0400

    Added the Admin Model class

---
+++ CampRealEstate/Models/Admin.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampRealEstate.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name:")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telephone Number:")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Login Id")]
+        [ForeignKey("Identityuser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+
+
+    }
+}

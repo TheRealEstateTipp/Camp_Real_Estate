64e146e6dc0a153e3b4600eef81b416308e6cac0
unknown <mstiff1983@gmail.com>
2020-07-17 17:34:22 -0400

    Created an Index method on the Admin Controller

--- CampRealEstate/Controllers/AdminController.cs
+++ CampRealEstate/Controllers/AdminController.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using CampRealEstate.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CampRealEstate.Controllers
-{   [Authorize(Roles ="Admin")]
+{   
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
         private ApplicationDbContext _context;
@@ -18,7 +20,15 @@ namespace CampRealEstate.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var admin = _context.Clients.Where(c => c.IdentityUserId == userId).FirstOrDefault();
+
+            if (admin == null)
+            {
+                return RedirectToAction("Create");
+            }
+
+            return View(admin);
         }
     }
 }
